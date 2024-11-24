a8b274e6016481f05ee035681de3979236a39f22
elton-barbosa <eltonbarbosasantos82@gmail.com>
2024-11-24 18:35:45 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const list = document.querySelector("ul")
+const buttonShowAll = document.querySelector(".show-all")
+const buttonMapAll = document.querySelector(".map-all")
+const somAll = document.querySelector(".som-all")
+const filterAll = document.querySelector(".filter-all")
+
+function formatCurrency(value) {
+        const newValue = value.toLocaleString('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+        });
+
+
+
+        return newValue
+}
+function showAll(productsArray) {
+
+        let myLi = ""
+        productsArray.forEach(product => {
+                myLi += `
+                   
+                    
+                        <li>
+                                <img src=${product.src}>
+                                <p>${product.name}</p>
+                                <p class="intem-price">R$ ${formatCurrency(product.price)}</p>
+                
+                        </li>
+                    
+                    `
+
+        })
+
+        list.innerHTML = myLi
+
+}
+
+function MapAllItems() {
+        const newPrices = menuOptions.map((product) => ({
+
+                ...product,
+                price: product.price * 0.9,
+
+        }))
+
+        showAll(newPrices)
+
+}
+
+function somAllItems() {
+
+        const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+
+        list.innerHTML = `    
+            
+                        <li>
+                <p>o vamlor total dos itens é R$ ${formatCurrency(totalValue)}</p>
+                
+                        </li>
+                `
+
+}
+
+function filterAllItems() {
+
+        const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+        showAll(filterJustVegan)
+}
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions))
+buttonMapAll.addEventListener("click", MapAllItems)
+somAll.addEventListener("click", somAllItems)
+filterAll.addEventListener("click", filterAllItems)
+
+
+
+
+
+
